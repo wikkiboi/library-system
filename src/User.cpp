d6a1a11d8ac0d1463f0be4e8cef630fabe526ea0
wikkiboi <mofouyu@gmail.com>
2024-11-17 03:16:41 -0800

    Setup User class members and implement registerUser function

--- src/User.cpp
+++ src/User.cpp
@@ -7,11 +7,47 @@
 using namespace std;
 
 bool User::registerUser(const string& username, const string& password) {
+    if (username.empty()) {
+        return false;
+    }
+
+    ifstream readFile("data/users.csv");
+    string line;
+    while (getline(readFile, line)) {
+        istringstream stream(line);
+        string fileUsername;
+        getline(stream, fileUsername, ',');
+        if (fileUsername == username) {
+            cout << "Username already taken. Please choose a different username.";
+            return false;
+        }
+    }
+
+    if (password.length() < 8) {
+        return false;
+    }
+
+    bool hasUpper = false, hasLower = false, hasDigit = false;
+    for (char ch: password) {
+        if (isupper(ch)) {
+            hasUpper = true;
+        } else if (islower(ch)) {
+            hasLower = true;
+        } else if (isdigit(ch)) {
+            hasDigit = true;
+        }
+    }
+
+    if (!hasUpper || !hasLower || !hasDigit) {
+        return false;
+    }
+
     ofstream file("data/users.csv", ios::app);
     if (!file.is_open()) {
         cerr << "Error opening users.csv\n";
         return false;
     }
+
 
     static random_device rd;
     static mt19937 gen(rd());
@@ -24,7 +60,6 @@ bool User::registerUser(const string& username, const string& password) {
 
     file << username << "," << password << "," << libraryId << ",user\n";
     file.close();
-    cout << "User registered successfully.\n";
     cout << "Library ID: " << libraryId << endl;
     return true;
 }
