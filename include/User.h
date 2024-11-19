8d580153603742902e1221687212e96ff0f93074
Ghostmaster-Ui <asing326@ucr.edu>
2024-11-18 16:30:48 -0800

    Add Book Feature

--- include/User.h
+++ include/User.h
@@ -3,19 +3,23 @@
 
 #include <string>
 using namespace std;
+
 class User {
     private:
         string userId;
         string username;
         string password;
         string libraryId;
-        string filePath = "data/users.csv";
+        string filePath = "data/users.csv"; // Default file path for user data
     
     public:
+        // User-related functions
         bool registerUser(const string& username, const string& password);
         bool loginUser(const string& username, const string& password);
         void setFilePath(const string& filePath);
 
+        // Book-related function
+        bool addBook(const string& title, const string& author, const string& genre);
 };
 
 #endif
