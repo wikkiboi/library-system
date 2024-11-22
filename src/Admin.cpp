bfd1b263918d6586bcdeced9739b8b3bc57df9f2
thass004 <thass004@ucr.edu>
2024-11-21 17:26:40 -0800

    Fixed/Added some code in Admin and User class

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include "Book.h"
+#include "Catalog.h"
 
 bool Admin::addBook(const string& bookId, const string& title, const string& author, const string& genre, const string& subGenre, bool isAvailable) const {
     Book newBook;
@@ -12,17 +13,29 @@ bool Admin::addBook(const string& bookId, const string& title, const string& aut
     newBook.setGenre(genre);
     newBook.setSubGenre(subGenre);
     newBook.setAvailability(true);
+    newBook.setBookId(bookId);
+    return newBook.addToBookCatalog();
+}
+
+bool Admin::addBook(const string& title, const string& author, const string& genre, const string& subGenre, bool isAvailable) const {
+    Book newBook;
+    newBook.setTitle(title);
+    newBook.setAuthor(author);
+    newBook.setGenre(genre);
+    newBook.setSubGenre(subGenre);
+    newBook.setAvailability(true);
+    newBook.setBookId();
     return newBook.addToBookCatalog();
 }
 
 bool Admin::editBook(const string& bookId, const string& newTitle, const string& newAuthor, const string& newGenre, const string& newSubGenre, bool isAvailable) const {
-    Book book;
-    Book bookToUpdate = book.getBookFromCatalog(bookId);
+    Catalog catalog;
+    Book bookToUpdate = catalog.getBookFromCatalog(bookId);
 
     return bookToUpdate.updateBookDetails(newTitle, newAuthor, newGenre, newSubGenre);
 }
 
 bool Admin::deleteBook(const string& bookId) const {
-    Book book;
-    return book.deleteBookFromCatalog(bookId); 
+    Catalog catalog;
+    return catalog.deleteBookFromCatalog(bookId); 
 }
